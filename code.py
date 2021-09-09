540d5a00cf27db406cf76b6de15ca42cb6311571
Shea Silverman <s@ucf.edu>
2021-09-09 17:58:19 -0400

    working

--- code.py
+++ code.py
@@ -29,8 +29,11 @@ SABER_COLOR = RED
 btn = DigitalInOut(board.GP5)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
-
 prev_state = btn.value
+
+whack = DigitalInOut(board.GP15)
+whack.direction = Direction.INPUT
+whack.pull = Pull.UP
 
 i2s = audiobusio.I2SOut(board.GP27, board.GP28, board.GP26)
 
@@ -40,9 +43,9 @@ saber_on = audiomp3.MP3Decoder(data)
 data = open("on.mp3", "rb")
 saber_off = audiomp3.MP3Decoder(data)
 
-data = open("hum.mp3", "rb")
-saber_hum = audiomp3.MP3Decoder(data)
-
+data = open("Clash clash.mp3", "rb")
+saber_clash = audiomp3.MP3Decoder(data)
+lightsaber_active = False
 # led = digitalio.DigitalInOut(board.GP17)
 # led.direction = digitalio.Direction.OUTPUT
 # led.value = True
@@ -104,6 +107,20 @@ def start_lightsaber(sound):
 
     pixels.brightness = 0.7
     pixels.show()
+    time.sleep(sleep)
+
+    pixels.brightness = 0.8
+    pixels.show()
+    time.sleep(sleep)
+
+    pixels.brightness = 0.9
+    pixels.show()
+    time.sleep(sleep)
+
+    pixels.brightness = 1.0
+    pixels.show()
+    lightsaber_active = True
+    return lightsaber_active
 
 
 def stop_lightsaber(sound):
@@ -111,7 +128,7 @@ def stop_lightsaber(sound):
 
     i2s.play(sound)
 
-    pixels.brightness = 0.6
+    pixels.brightness = 0.7
     sleep = 0.05
     pixels.fill(SABER_COLOR)
     pixels.show()
@@ -142,20 +159,40 @@ def stop_lightsaber(sound):
 
     pixels.brightness = 0.0
     pixels.show()
+    lightsaber_active = False
+    return lightsaber_active
 
 while True:
-
+    print("lightsaber_active: ", lightsaber_active)
     cur_state = btn.value
     if cur_state != prev_state:
         if not cur_state:
             print("BTN is down")
-            start_lightsaber(saber_on)
+            lightsaber_active = start_lightsaber(saber_on)
             #idle_lightsaber(saber_on)
+
         else:
-            print("BTN is up")
-            stop_lightsaber(saber_off)
+          print("BTN is up")
+          lightsaber_active = stop_lightsaber(saber_off)
 
     prev_state = cur_state
+
+    if not whack.value and lightsaber_active == True:
+      print("whack is down")
+      i2s.stop()
+      i2s.play(saber_clash)
+      pixels.fill(BLUE)
+      pixels.brightness = 0.2
+      pixels.show()
+      time.sleep(1)
+      pixels.brightness = 0.0
+      pixels.show()
+
+
+    if lightsaber_active == True:
+      pixels.fill(SABER_COLOR)
+      pixels.brightness = 1.0
+      pixels.show()
     # led.value = False
 
     # pixels.fill(RED)
